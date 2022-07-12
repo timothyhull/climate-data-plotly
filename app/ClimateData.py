bdfed02badb1e6b4ea0a2d88e95fe3cef58cdf3c
Timothy Hull <timothyhull@gmail.com>
2022-07-12 23:56:14 +0000

    Initial commit

---
+++ app/ClimateData.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" ClimateData class and methods. """
+
+# Imports - Python Standard Library
+from typing import Dict
+
+# Imports - Third-Party
+# from requests import get
+# from requests.exceptions import HTTPError
+
+# Imports - Local
+
+# Constants
+
+
+class ClimateData:
+    """ Climate Data class object. """
+
+    def __init__(self) -> None:
+        """ ClimateData initialization method:
+
+            Args:
+                None.
+
+            Returns:
+                None.
+        """
+
+        return None
+
+    def get_climate_data(self) -> Dict:
+        """ ClimateData initialization method:
+
+            Args:
+                None.
+
+            Returns:
+                climate_data (Dict):
+                    Climate data
+        """
+
+        return None
---
+++ app/climate_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Climate Data App Main Application. """
+
+# Imports - Python Standard Library
+
+# Imports - Third-Party
+
+# Imports - Local
